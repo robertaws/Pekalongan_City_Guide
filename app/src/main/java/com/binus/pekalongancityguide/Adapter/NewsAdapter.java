d169c060bfc2f553fabddf81aaa07743004d8189
robertaws <durswijh@gmail.com>
2023-03-28 12:28:54 +0700

    add news details

--- app/src/main/java/com/binus/pekalongancityguide/Adapter/NewsAdapter.java
+++ app/src/main/java/com/binus/pekalongancityguide/Adapter/NewsAdapter.java
@@ -1,5 +1,6 @@
 package com.binus.pekalongancityguide.Adapter;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.binus.pekalongancityguide.Layout.NewsDetail;
 import com.binus.pekalongancityguide.R;
+import com.binus.pekalongancityguide.databinding.ListDestinationBinding;
+import com.binus.pekalongancityguide.databinding.ListNewsBinding;
 import com.bumptech.glide.Glide;
 import com.kwabenaberko.newsapilib.models.Article;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     private List<Article> mArticles;
+    private static ListNewsBinding binding;
 
     public NewsAdapter(List<Article> articles) {
         this.mArticles = articles;
@@ -27,63 +37,71 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_news, parent, false);
-        return new ViewHolder(view);
+        binding = ListNewsBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new ViewHolder(binding.getRoot());
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Article article = mArticles.get(position);
-
-        // Set article title
-        holder.titleTextView.setText(article.getTitle());
-
-        // Set article description
-        holder.descriptionTextView.setText(article.getDescription());
-
-        // Set article author
-        holder.authorTextView.setText(article.getAuthor());
-
-        // Set article source name
-        holder.sourceTextView.setText(article.getSource().getName());
-
-        // Set article published date
-        holder.dateTextView.setText(article.getPublishedAt());
-
-        // Set article image
+        holder.titleTV.setText(article.getTitle());
+        holder.sourceTV.setText(article.getSource().getName());
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, -28);
+        String publishedDateString = article.getPublishedAt();
+        try {
+            Date publishedDate = inputDateFormat.parse(publishedDateString);
+            String formattedDate = outputDateFormat.format(publishedDate);
+            holder.dateTV.setText(formattedDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         if (article.getUrlToImage() != null) {
             Glide.with(holder.itemView.getContext())
                     .load(article.getUrlToImage())
-                    .into(holder.imageView);
+                    .into(holder.newsIV);
         }
-
         Log.d("NewsAPI", "Title: " + article.getTitle());
         Log.d("NewsAPI", "Description: " + article.getDescription());
         Log.d("NewsAPI", "Image URL: " + article.getUrlToImage());
 
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), NewsDetail.class);
+            intent.putExtra("title", article.getTitle());
+            intent.putExtra("desc", article.getDescription());
+            intent.putExtra("imageUrl", article.getUrlToImage());
+            intent.putExtra("source", article.getSource().getName());
+            intent.putExtra("newsUrl", article.getUrl());
+            SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault());
+            SimpleDateFormat output = new SimpleDateFormat("d MMMM yyyy", new Locale("id", "ID"));
+            Date publishedDate = null;
+            try {
+                publishedDate = input.parse(article.getPublishedAt());
+            }catch (ParseException e) {
+                e.printStackTrace();
+            }
+            String formattedDate = output.format(publishedDate);
+            intent.putExtra("date", formattedDate);
+            intent.putExtra("author",article.getAuthor());
+            v.getContext().startActivity(intent);
+        });
     }
-
     @Override
     public int getItemCount() {
         return mArticles.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView titleTextView;
-        private TextView descriptionTextView;
-        private TextView authorTextView;
-        private TextView sourceTextView;
-        private TextView dateTextView;
-        private ImageView imageView;
-
+        private TextView titleTV,dateTV,sourceTV;
+        private ImageView newsIV;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            titleTextView = itemView.findViewById(R.id.news_title);
-            descriptionTextView = itemView.findViewById(R.id.news_body);
-            authorTextView = itemView.findViewById(R.id.news_author);
-            sourceTextView = itemView.findViewById(R.id.news_source);
-            dateTextView = itemView.findViewById(R.id.news_date);
-            imageView = itemView.findViewById(R.id.news_iv);
+            titleTV = binding.newsTitle;
+            dateTV = binding.newsDate;
+            sourceTV = binding.newsSource;
+            newsIV = binding.newsIv;
         }
     }
 }
