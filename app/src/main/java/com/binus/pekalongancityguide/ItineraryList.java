18990e5ef5f2d7a3ba6d80fc2846bd9195b2a9a5
robertaws <durswijh@gmail.com>
2023-04-07 17:08:07 +0700

    add something

--- app/src/main/java/com/binus/pekalongancityguide/ItineraryList.java
+++ app/src/main/java/com/binus/pekalongancityguide/ItineraryList.java
@@ -1,14 +1,48 @@
 package com.binus.pekalongancityguide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ItineraryList extends AppCompatActivity {
+import com.binus.pekalongancityguide.databinding.ActivityItineraryListBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class ItineraryList extends AppCompatActivity {
+    private ActivityItineraryListBinding binding;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_itinerary_list);
+        binding = ActivityItineraryListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadItinerary();
+    }
+    private void loadItinerary(){
+        DatabaseReference reference = FirebaseDatabase.getInstance("https://pekalongan-city-guide-5bf2e-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("Users");
+        reference.child(firebaseAuth.getUid())
+                .child("itinerary")
+                .child("destiId").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String date = "" + snapshot.child("date").getValue();
+                        String startTime = "" + snapshot.child("startTime").getValue();
+                        String endTime = "" + snapshot.child("endTime").getValue();
+                        String placeId = "" + snapshot.child("placeId").getValue();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
     }
 }
