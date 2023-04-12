439f183db134ddb46f6cf6645df78d3c93bfa72f
Meidie <46555331+meidielo@users.noreply.github.com>
2023-04-12 14:15:08 +0700

    now the itinerary shows distance

--- app/src/main/java/com/binus/pekalongancityguide/ItemTemplate/Itinerary.java
+++ app/src/main/java/com/binus/pekalongancityguide/ItemTemplate/Itinerary.java
@@ -1,48 +1,53 @@
 package com.binus.pekalongancityguide.ItemTemplate;
 
 public class Itinerary {
-    private String date,endTime,placeId,startTime;
-    public Itinerary() {
-    }
-    public Itinerary(String date, String endTime, String placeId, String startTime) {
+    private final String date;
+    private final String endTime;
+    private final String placeName;
+    private final String startTime;
+    private final double latitude;
+    private final double longitude;
+    private float distance;
+
+    public Itinerary(String date, String endTime, String placeName, String startTime, double latitude, double longitude, float distance) {
         this.date = date;
         this.endTime = endTime;
-        this.placeId = placeId;
+        this.placeName = placeName;
         this.startTime = startTime;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.distance = distance;
     }
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getPlaceId() {
-        return placeId;
-    }
-
-    public void setPlaceId(String placeId) {
-        this.placeId = placeId;
+    public String getPlaceName() {
+        return placeName;
     }
 
     public String getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
+    public double getLatitude() {
+        return latitude;
     }
 
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
 }
-
-
