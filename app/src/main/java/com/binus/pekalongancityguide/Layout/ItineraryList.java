fedd70d6506f31456341612208a365734cde0813
Meidie <46555331+meidielo@users.noreply.github.com>
2023-04-15 16:49:48 +0700

    added the pager

--- app/src/main/java/com/binus/pekalongancityguide/Layout/ItineraryList.java
+++ app/src/main/java/com/binus/pekalongancityguide/Layout/ItineraryList.java
@@ -3,21 +3,18 @@ package com.binus.pekalongancityguide.Layout;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import com.binus.pekalongancityguide.R;
+import com.binus.pekalongancityguide.ItemTemplate.Itinerary;
 import com.binus.pekalongancityguide.databinding.ActivityItineraryListBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -27,155 +24,92 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
-import static android.content.ContentValues.TAG;
-
-public class ItineraryList extends AppCompatActivity {
+public class ItineraryList extends Fragment {
     public ActivityItineraryListBinding binding;
     private final FirebaseDatabase database = FirebaseDatabase.getInstance("https://pekalongan-city-guide-5bf2e-default-rtdb.asia-southeast1.firebasedatabase.app/");
     private FirebaseAuth firebaseAuth;
+    public ArrayList<Itinerary> itineraryArrayList;
+    public ItineraryList.ViewPagerAdapter viewPagerAdapter;
+    private static final String TAG = "DESTI_USER_TAG";
+
+    public ItineraryList() {
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = ActivityItineraryListBinding.inflate(inflater, container, false);
+        setupViewPagerAdapter(binding.viewPager);
+        binding.itineraryTab.setupWithViewPager(binding.viewPager);
         firebaseAuth = FirebaseAuth.getInstance();
-        binding = ActivityItineraryListBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        return binding.getRoot();
+    }
 
-        binding.backtoprofile.setOnClickListener(v -> {
-            onBackPressed();
-        });
-
-        // Fetch data from Firebase and create list of dates
-        DatabaseReference userRef = database.getReference("Users").child(Objects.requireNonNull(firebaseAuth.getUid()));
-        Query itineraryQuery = userRef.child("itinerary");
+    private void setupViewPagerAdapter(ViewPager viewPager) {
+        viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager(), getContext());
+        itineraryArrayList = new ArrayList<>();
+        DatabaseReference reference = database.getReference().child(firebaseAuth.getUid());
+        Query itineraryQuery = reference.child("itinerary");
         itineraryQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                List<String> dates = new ArrayList<>();
-                for (DataSnapshot itinerarySnapshot : snapshot.getChildren()) {
-                    String date = itinerarySnapshot.child("date").getValue(String.class);
-                    if (date != null && !date.isEmpty()) {
-                        Log.d(TAG, "date: " + date);
-                        date = convertToIso8601(date);
-                        dates.add(date);
-                    }
+                itineraryArrayList.clear();
+                viewPagerAdapter.notifyDataSetChanged();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Itinerary itinerary = dataSnapshot.getValue(Itinerary.class);
+                    itineraryArrayList.add(itinerary);
+                    viewPagerAdapter.addFragment(ItineraryFragment.newInstance(
+                            "" + itinerary.getDate(),
+                            "" + itinerary.getUid()), itinerary.getDate()); //TODO change to DAY #
+                    viewPagerAdapter.notifyDataSetChanged();
                 }
-
-                List<Fragment> fragments = createFragmentsList(dates);
-
-                ItineraryPagerAdapter vpAdapter = new ItineraryPagerAdapter(ItineraryList.this, getSupportFragmentManager(), fragments, dates);
-                binding.viewPager.setAdapter(vpAdapter);
-                binding.itineraryTab.setupWithViewPager(binding.viewPager);
-                binding.itineraryTab.setSelectedTabIndicatorColor(ContextCompat.getColor(ItineraryList.this, R.color.white));
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Handle error
+
             }
         });
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setOffscreenPageLimit(1);
     }
 
-    public class ItineraryPagerAdapter extends FragmentPagerAdapter {
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final ArrayList<ItineraryFragment> fragmentList = new ArrayList<>();
+        private final ArrayList<String> fragmentTitleList = new ArrayList<>();
+        private final Context context;
 
-        private final List<Fragment> fragments;
-        private final List<String> dates;
-
-        public ItineraryPagerAdapter(Context context, FragmentManager fm, List<Fragment> fragments, List<String> dates) {
-            super(fm);
-            this.fragments = fragments;
-            this.dates = dates;
+        public ViewPagerAdapter(FragmentManager fm, Context context) {
+            super(fm, BEHAVIOR_SET_USER_VISIBLE_HINT);
+            this.context = context;
         }
 
+        @NonNull
         @Override
         public Fragment getItem(int position) {
-            return fragments.get(position);
+            return fragmentList.get(position);
         }
 
         @Override
         public int getCount() {
-            return fragments.size();
+            return fragmentList.size();
         }
 
-        @Override
+        private void addFragment(ItineraryFragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
         public CharSequence getPageTitle(int position) {
-            return dates.get(position);
-        }
-    }
-
-    public static class EmptyFragment extends Fragment {
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_empty, container, false);
-        }
-    }
-
-    private List<Fragment> createEmptyFragments(List<String> emptyDates) {
-        List<Fragment> fragments = new ArrayList<>();
-        for (String date : emptyDates) {
-            fragments.add(new EmptyFragment());
-        }
-        return fragments;
-    }
-
-    private List<Fragment> createFragmentsList(List<String> dates) {
-        List<Fragment> fragments = new ArrayList<>();
-        List<String> emptyDates = new ArrayList<>();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        Date prevDate = null;
-        int dayCount = 1; // initialize the day count to 1
-        for (String date : dates) {
-            try {
-                Date currentDate = dateFormat.parse(date);
-                if (prevDate != null) {
-                    long diffInMs = currentDate.getTime() - prevDate.getTime();
-                    long diffInDays = TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS);
-                    if (diffInDays > 1) {
-                        for (int i = 1; i < diffInDays; i++) {
-                            String emptyDate = dateFormat.format(new Date(prevDate.getTime() + i * TimeUnit.DAYS.toMillis(1)));
-                            emptyDates.add(emptyDate);
-                        }
-                    }
-                }
-                ItineraryFragment fragment = new ItineraryFragment();
-                Bundle args = new Bundle();
-                args.putInt("dayIndex", dayCount); // pass the index of the date in the list
-                fragment.setArguments(args);
-                fragments.add(fragment);
-                prevDate = currentDate;
-                Log.d(TAG, "current date: " + currentDate);
-                dayCount++; // increment the day count
-            } catch (ParseException e) {
-                Log.e("ItineraryList", "Error parsing date: " + date, e);
-                Toast.makeText(ItineraryList.this, "Error parsing date: " + date, Toast.LENGTH_SHORT).show();
-            }
-        }
-        fragments.addAll(createEmptyFragments(emptyDates));
-        dates.addAll(emptyDates);
-        return fragments;
-    }
-
-    private String convertToIso8601(String dateStr) {
-        try {
-            DateFormat originalDateFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault());
-            Date date = originalDateFormat.parse(dateStr);
-            DateFormat targetDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-            return targetDateFormat.format(date);
-        } catch (ParseException e) {
-            Log.e("ItineraryList", "Error parsing date: " + dateStr, e);
-            Toast.makeText(ItineraryList.this, "Error parsing date: " + dateStr, Toast.LENGTH_SHORT).show();
-            return "";
+            return fragmentTitleList.get(position);
         }
     }
 }
