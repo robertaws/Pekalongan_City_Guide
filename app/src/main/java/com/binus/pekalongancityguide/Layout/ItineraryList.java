35c029a0cd1eb9c31d89694cb0ce815f9c2afa8a
Meidie <46555331+meidielo@users.noreply.github.com>
2023-04-17 16:02:46 +0700

    fixed the pager

--- app/src/main/java/com/binus/pekalongancityguide/Layout/ItineraryList.java
+++ app/src/main/java/com/binus/pekalongancityguide/Layout/ItineraryList.java
@@ -4,13 +4,9 @@ package com.binus.pekalongancityguide.Layout;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -35,7 +31,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 import static android.content.ContentValues.TAG;
 
@@ -113,56 +108,22 @@ public class ItineraryList extends AppCompatActivity {
         }
     }
 
-    public static class EmptyFragment extends Fragment {
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_empty, container, false);
-        }
-    }
-
-    private List<Fragment> createEmptyFragments(List<String> emptyDates) {
-        List<Fragment> fragments = new ArrayList<>();
-        for (String date : emptyDates) {
-            fragments.add(new EmptyFragment());
-        }
-        return fragments;
-    }
-
     private List<Fragment> createFragmentsList(List<String> dates) {
         List<Fragment> fragments = new ArrayList<>();
-        List<String> emptyDates = new ArrayList<>();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        Date prevDate = null;
-        int dayCount = 1; // initialize the day count to 1
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM", Locale.getDefault());
         for (String date : dates) {
             try {
                 Date currentDate = dateFormat.parse(date);
-                if (prevDate != null) {
-                    long diffInMs = currentDate.getTime() - prevDate.getTime();
-                    long diffInDays = TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS);
-                    if (diffInDays > 1) {
-                        for (int i = 1; i < diffInDays; i++) {
-                            String emptyDate = dateFormat.format(new Date(prevDate.getTime() + i * TimeUnit.DAYS.toMillis(1)));
-                            emptyDates.add(emptyDate);
-                        }
-                    }
-                }
                 ItineraryFragment fragment = new ItineraryFragment();
                 Bundle args = new Bundle();
-                args.putInt("dayIndex", dayCount); // pass the index of the date in the list
+                args.putString("date", date);
                 fragment.setArguments(args);
                 fragments.add(fragment);
-                prevDate = currentDate;
-                Log.d(TAG, "current date: " + currentDate);
-                dayCount++; // increment the day count
             } catch (ParseException e) {
                 Log.e("ItineraryList", "Error parsing date: " + date, e);
                 Toast.makeText(ItineraryList.this, "Error parsing date: " + date, Toast.LENGTH_SHORT).show();
             }
         }
-        fragments.addAll(createEmptyFragments(emptyDates));
-        dates.addAll(emptyDates);
         return fragments;
     }
 
@@ -170,7 +131,7 @@ public class ItineraryList extends AppCompatActivity {
         try {
             DateFormat originalDateFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault());
             Date date = originalDateFormat.parse(dateStr);
-            DateFormat targetDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            DateFormat targetDateFormat = new SimpleDateFormat("dd MMMM", Locale.getDefault());
             return targetDateFormat.format(date);
         } catch (ParseException e) {
             Log.e("ItineraryList", "Error parsing date: " + dateStr, e);
