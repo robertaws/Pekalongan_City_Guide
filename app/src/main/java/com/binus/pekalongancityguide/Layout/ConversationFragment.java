a4ff88e64f5d66f71e3934ebbdd73708dc2dda06
robertaws <durswijh@gmail.com>
2023-04-24 16:21:41 +0700

    fix smtg

--- app/src/main/java/com/binus/pekalongancityguide/Layout/ConversationFragment.java
+++ app/src/main/java/com/binus/pekalongancityguide/Layout/ConversationFragment.java
@@ -1,10 +1,13 @@
 package com.binus.pekalongancityguide.Layout;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
