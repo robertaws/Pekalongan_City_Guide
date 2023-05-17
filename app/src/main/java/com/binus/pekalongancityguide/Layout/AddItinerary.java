d53a7a1764cd873349db02ad1b35aee9cb8a52f9
Meidie <46555331+meidielo@users.noreply.github.com>
2023-05-17 17:17:36 +0700

    added the add itinerary button add dialog item itinerary ok

--- app/src/main/java/com/binus/pekalongancityguide/Layout/AddItinerary.java
+++ app/src/main/java/com/binus/pekalongancityguide/Layout/AddItinerary.java
@@ -1,5 +1,9 @@
 package com.binus.pekalongancityguide.Layout;
 
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,16 +14,22 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.binus.pekalongancityguide.Adapter.IterAdapter;
 import com.binus.pekalongancityguide.ItemTemplate.Destination;
 import com.binus.pekalongancityguide.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,7 +47,7 @@ public class AddItinerary extends Fragment implements IterAdapter.OnItemLongClic
     private RecyclerView iterRV;
     private Button addIter;
     private RelativeLayout selectLayout;
-    private TextView selectTv;
+    private TextView selectTv, dialogTitle, distanceTv, durationTv;
     private ImageButton selectCancel;
     public ArrayList<Destination> destinationArrayList, selectedItems;
     private View view;
@@ -109,11 +119,83 @@ public class AddItinerary extends Fragment implements IterAdapter.OnItemLongClic
         } else {
             loadCategoriedDestination();
         }
-        addIter.setOnClickListener(v -> {
-            Log.d(TAG, "start: " + startDate + " end : " + endDate);
-        });
+        addIter.setOnClickListener(v -> showInputDialog());
         selectCancel.setOnClickListener(v -> iterAdapter.exitSelectMode());
         return view;
+    }
+
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        // Set the custom layout for the dialog
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_input_details, null);
+        builder.setView(view);
+        // Get a reference to the container layout where the EditText fields will be added
+        LinearLayout containerLayout = view.findViewById(R.id.container_layout);
+        Button addBtn = view.findViewById(R.id.add_iter_button);
+        dialogTitle = view.findViewById(R.id.dialog_title);
+
+        // Get the selected items from the adapter
+        ArrayList<Destination> selectedItems = iterAdapter.getSelectedItems();
+        // Iterate through the selected items and add EditText fields for each item
+        for (int i = 0; i < selectedItems.size(); i++) {
+            View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_input_details, null);
+            dialogTitle.setText("Pick your first destination");
+            Destination selectedItem = selectedItems.get(i);
+            CardView cardView = itemView.findViewById(R.id.add_item_cardview);
+            LinearLayout layoutBG = itemView.findViewById(R.id.layout_bg);
+            TextView placeText = itemView.findViewById(R.id.placeNameTextView);
+            durationTv = itemView.findViewById(R.id.durationTextView);
+            distanceTv = itemView.findViewById(R.id.distanceTextView);
+            placeText.setText(selectedItem.getTitle());
+            distanceTv.setText(String.valueOf(selectedItem.getDistance()));
+
+            CardView.LayoutParams layoutParams = new CardView.LayoutParams(
+                    CardView.LayoutParams.MATCH_PARENT,
+                    CardView.LayoutParams.WRAP_CONTENT
+            );
+            layoutParams.setMargins(0, 10, 0, 10);
+            cardView.setLayoutParams(layoutParams);
+
+            containerLayout.addView(itemView);
+
+            // Load the image using Glide and set it as the background of layoutBG
+            loadImage(selectedItem, layoutBG);
+        }
+        AlertDialog dialog = builder.create();
+        addBtn.setOnClickListener(v -> {
+            // Iterate through the container layout and retrieve the input data for each item
+            for (int i = 0; i < containerLayout.getChildCount(); i++) {
+                View itemView = containerLayout.getChildAt(i);
+                // Handle the input data here, for example, save it or perform any necessary actions
+                // with the selected items and the entered details
+            }
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
+
+    private void loadImage(Destination destination, LinearLayout layoutBG) {
+        String imageUrl = destination.getUrl();
+        Glide.with(layoutBG.getContext())
+                .asBitmap()
+                .load(imageUrl)
+                .placeholder(R.drawable.logo)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        // Set the image bitmap as the background of layoutBG
+                        BitmapDrawable drawable = new BitmapDrawable(layoutBG.getResources(), resource);
+                        layoutBG.setBackground(drawable);
+                    }
+
+                    @Override
+                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                        super.onLoadFailed(errorDrawable);
+                        Log.d(TAG, "on Failure: failed to get file from URL");
+                    }
+                });
     }
 
     private void init() {
@@ -123,6 +205,7 @@ public class AddItinerary extends Fragment implements IterAdapter.OnItemLongClic
         selectLayout = view.findViewById(R.id.select_layout);
         selectCancel = view.findViewById(R.id.select_cancel);
     }
+
     private void loadDestinations() {
         destinationArrayList = new ArrayList<>();
         DatabaseReference reference = FirebaseDatabase.getInstance("https://pekalongan-city-guide-5bf2e-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Destination");
