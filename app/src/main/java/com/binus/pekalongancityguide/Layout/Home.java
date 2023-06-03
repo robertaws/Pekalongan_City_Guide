f1389fd0981fb4d5901e7b77cb9e4781773319d9
Meidie <46555331+meidielo@users.noreply.github.com>
2023-06-03 14:22:39 +0700

    fixed the itinerary and bookmark for guest account

--- app/src/main/java/com/binus/pekalongancityguide/Layout/Home.java
+++ app/src/main/java/com/binus/pekalongancityguide/Layout/Home.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentManager;
 
 import com.binus.pekalongancityguide.R;
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity{
     private static final int home = 1;
@@ -23,12 +24,14 @@ public class Home extends AppCompatActivity{
     private static final int bm = 4;
     private static final int convo = 5;
     private static final int pr = 6;
+    FirebaseAuth firebaseAuth;
     MeowBottomNavigation bottomNavigationView;
     private boolean doubleTap = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        firebaseAuth = FirebaseAuth.getInstance();
         bottomNavigationView = findViewById(R.id.bottom_navi);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
         bottomNavigationView.add(new MeowBottomNavigation.Model(home, R.drawable.ic_home));
@@ -43,15 +46,23 @@ public class Home extends AppCompatActivity{
                 case home:
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commitAllowingStateLoss();
                     break;
-                    
+
                 case desti:
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, destinationPager).commitAllowingStateLoss();
                     break;
                 case iter:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, itineraryList).commitAllowingStateLoss();
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        Toast.makeText(Home.this, R.string.notLogin, Toast.LENGTH_SHORT).show();
+                    } else {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, itineraryList).commitAllowingStateLoss();
+                    }
                     break;
                 case bm:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, bookmarkFragment).commitAllowingStateLoss();
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        Toast.makeText(Home.this, R.string.notLogin, Toast.LENGTH_SHORT).show();
+                    } else {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, bookmarkFragment).commitAllowingStateLoss();
+                    }
                     break;
 
                 case convo:
