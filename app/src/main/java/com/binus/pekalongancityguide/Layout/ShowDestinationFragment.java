7dc743bf17d62af4df228671c0abf3ef5539b573
Meidie <46555331+meidielo@users.noreply.github.com>
2023-06-17 12:46:20 +0700

    fixed the fixed routes location dialog

--- app/src/main/java/com/binus/pekalongancityguide/Layout/ShowDestinationFragment.java
+++ app/src/main/java/com/binus/pekalongancityguide/Layout/ShowDestinationFragment.java
@@ -273,7 +273,7 @@ public class ShowDestinationFragment extends Fragment {
                 Log.e(TAG, "An error occurred: " + status);
             }
         });
-        locBinding.useCurLoc.setOnClickListener(v -> {
+        locBinding.usecurloc.setOnClickListener(v -> {
             if (getContext() != null && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                     ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_LOCATION);
@@ -321,7 +321,7 @@ public class ShowDestinationFragment extends Fragment {
                 });
             }
         });
-        locBinding.setLocBtn.setOnClickListener(v -> {
+        locBinding.setlocbtn.setOnClickListener(v -> {
             binding.changeLoc.setText(addressString);
             dialog.dismiss();
             updateDistances();
@@ -329,7 +329,7 @@ public class ShowDestinationFragment extends Fragment {
             AppCompatActivity appCompatActivity = (AppCompatActivity) getContext();
             FragmentManager fragmentManager = appCompatActivity.getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.container,pager);
+            fragmentTransaction.replace(R.id.container, pager);
             fragmentTransaction.commit();
             if (coordinate != null) {
                 SharedPreferences.Editor editor = prefs.edit();
